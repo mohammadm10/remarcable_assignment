3191e343a268dc3d64961b70787cf66b5a633b66
mohammadm10 <mohammadali_10@hotmail.com>
2026-03-04 17:21:50 -0800

    Enhance product filtering functionality with search, category and tag support

--- products/views.py
+++ products/views.py
@@ -5,20 +5,41 @@ from .models import Product, Category, Tag
 def extract_filters_from_request(request):
     search_query = request.GET.get('search', '')
     category_id = request.GET.get('category')
-    tag_id = request.GET.get('tag')
+    tags = request.GET.getlist('tag')
+    all_tag_ids = [tag_id for tag_id in tags]
+    
+    return search_query, category_id, all_tag_ids
 
-    return search_query, category_id, tag_id
+def apply_product_filters(products, search_query, category_id, tag_id):
+    if search_query:
+        # Case-insensitive search in description
+        products = products.filter(description__icontains=search_query)
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if tag_id:
+        # Use in operator to filter products that have any of the selected tags
+        products = products.filter(tags__id__in=tag_id)
+
+    return products
+
+def get_all_static_data():
+
+    p = Product.objects.all()
+    c = Category.objects.all()
+    t = Tag.objects.all()
+
+    return p, c, t
 
 def product_list(request):
     template = loader.get_template('products/product_list.html')
 
-    product_search_query, selected_category_id, selected_tag_id = extract_filters_from_request(request)
+    product_search_query, selected_category_id, selected_tag_ids = extract_filters_from_request(request)
 
-    print(f"Search Query: {product_search_query}, Category ID: {selected_category_id}, Tag ID: {selected_tag_id}")
+    all_products, all_categories, all_tags = get_all_static_data()
 
-    all_products = Product.objects.all()
-    all_categories = Category.objects.all()
-    all_tags = Tag.objects.all()
+    all_products = apply_product_filters(all_products, product_search_query, selected_category_id, selected_tag_ids)
 
     context = {
         'products': all_products,
